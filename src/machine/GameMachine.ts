6a04ab3a5527e2c9fa0e60122515557950e7912e
Z-kamili <zakariakamili97@gmail.com>
2022-10-06 11:54:00 +0100

    create etats

---
+++ src/machine/GameMachine.ts
@@ -0,0 +1,50 @@
+import {createMachine} from 'xstate';
+
+enum GameStates {
+
+    LOBBY = 'LOBBY',
+    PLAY = 'PLAY',
+    VICTORY = 'VICTORY',
+    DRAW = 'DRAW'
+
+}
+ 
+export const GameMachine = createMachine({
+
+   // etats management 
+     id: 'game',
+     initial: GameStates.LOBBY,
+     states : {
+        [GameStates.LOBBY] : {
+            on : {
+                join : {
+                    target: GameStates.LOBBY
+                },
+                leave : {
+                    target : GameStates.LOBBY
+                },
+                chooseColor: {
+                    target:GameStates.LOBBY
+                },
+                start : {
+                    target: GameStates.PLAY
+                },
+            }
+     },
+     [GameStates.PLAY] : {
+        on : {
+            dropToken:{
+                target:'???'
+            }
+        }
+     },
+     [GameStates.VICTORY]: {
+        on: {
+            restart: {
+                target: GameStates.LOBBY
+            }
+        }
+     }
+
+}
+})
